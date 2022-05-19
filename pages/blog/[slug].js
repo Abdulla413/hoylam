cb8b938198ae91e2ad5e63d27fc51987c0c72eb7
abdulla <abdulla.arish@protonmail.com>
2022-05-20 01:16:54 +0200

    img tag changed to react Image hock

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -17,23 +17,26 @@ export default function PostPage ({frontmatter:{title, category, date, cover_ima
        
        <Link href="/blog">Go Back</Link>
        <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
-           <div className="flex justify-between items-center mt-4">
-           <h1 className="text-5xl mb-7 "> {title}</h1>
-           <CategoryLabel> 
-               {category }
-           </CategoryLabel>
-           </div>
-           <Image src={cover_image} alt="" className="w-full rounded"/>
+            <div className='flex justify-between items-center mt-4'>
+               
+              <h1 className='text-5xl mb-7'>{title}</h1>
+                 <CategoryLabel>{category}</CategoryLabel>
+                 </div>
+            
+
+<Image src={cover_image} alt=""  height={420}    width={600} className=" rounded"/>
+            
            
-           <div className="flex justify-between items-center bg-gray-100 p-2 my-8 rounded">
+           
+            <div className="flex justify-between items-center bg-gray-100 p-2 my-8 rounded">
                <div className="flex items-center">
-                   <Image src={author_image} alt="" className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block" />
+                   <Image src={author_image} alt=""  height={20} width={20} className="mx-4 object-cover rounded-full hidden sm:block" />
                    <h4>{author}</h4>
                </div>
                <div className="mr-4">
                    {date}
                </div>
-           </div>
+          </div>
            <div className="blog-text mt-2">
                <div dangerouslySetInnerHTML={{__html:marked(content)}}></div>
            </div>
